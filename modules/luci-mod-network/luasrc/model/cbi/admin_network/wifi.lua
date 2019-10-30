d454dfc09390263f1525e52719e51e2dcfcc830c
wifimedia <dungtd8x@gmail.com>
2019-10-30 16:05:52 +0700

    add Max Clients WIFI

--- modules/luci-mod-network/luasrc/model/cbi/admin_network/wifi.lua
+++ modules/luci-mod-network/luasrc/model/cbi/admin_network/wifi.lua
@@ -495,6 +495,9 @@ if hwtype == "mac80211" then
 			return mode
 		end
 	end
+	maxassoc = s:taboption("general", Value, "maxassoc", translate("Max Clients"))
+	maxassoc:depends({mode="ap"})
+	maxassoc:depends({mode="ap-wds"})
 
 	hidden = s:taboption("general", Flag, "hidden", translate("Hide <abbr title=\"Extended Service Set Identifier\">ESSID</abbr>"))
 	hidden:depends({mode="ap"})
